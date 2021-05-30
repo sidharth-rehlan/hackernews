a619ebc33c0fed1ee65f40f7809dc56aed4b23ae
sidharth <sidharth@nagarro.com>
2021-05-30 16:10:32 +0530

    commit pagination styling

---
+++ src/pages/Home.jsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import NewsDetail from "../components/newsDetail";
+import UpVoteChart from "../components/upVoteChart/UpVoteChart";
+import config from "../config";
+
+function Home() {
+  const [pagination, setPagination] = useState({});
+  const [news, setNews] = useState([]);
+
+  const fetchNewsFeed = async () => {
+    const response = await axios.get(`${config.baseUrl}${config.newsFeed}`);
+    console.log("response....", response);
+    if (response.status === 200) {
+      console.log(response);
+      return response.data;
+    }
+  };
+
+  useEffect(async () => {
+    let storedHideData = localStorage.getItem("hideInfo");
+    let hideData = storedHideData === null ? {} : JSON.parse(storedHideData);
+
+    let storedVoteData = localStorage.getItem("voteUp");
+    let voteData = storedVoteData === null ? {} : JSON.parse(storedVoteData);
+
+    const data = await fetchNewsFeed();
+    console.log(data);
+    setPagination({ currentPage: data.page, numberOfPages: data.nbPages });
+
+    const newsData = data.hits.map((hit) => {
+      const id = hit.objectID;
+      return {
+        url: hit.url,
+        created_at: hit.created_at,
+        points: hit.points,
+        title: hit.title,
+        id: id,
+        author: hit.author,
+        isHide: !!hideData[id],
+        voteUp: voteData[id] ? parseInt(voteData[id]) : 1,
+      };
+    });
+    setNews(newsData);
+  }, []);
+
+  const hideNewsHandler = (id) => {
+    let storedHideData = localStorage.getItem("hideInfo");
+    let hideData = storedHideData === null ? {} : JSON.parse(storedHideData);
+    hideData[id] = true;
+    localStorage.setItem("hideInfo", JSON.stringify(hideData));
+
+    let newsInfo = [...news];
+    let index = newsInfo.findIndex((news) => {
+      return news.id === id;
+    });
+    newsInfo[index] = { ...newsInfo[index], isHide: hideData[id] };
+    setNews(newsInfo);
+  };
+
+  const voteUpHandler = (id) => {
+    let storedVoteData = localStorage.getItem("voteUp");
+    let voteData = storedVoteData === null ? {} : JSON.parse(storedVoteData);
+    voteData[id] = voteData[id] ? voteData[id] + 1 : 1;
+    localStorage.setItem("voteUp", JSON.stringify(voteData));
+    let newsInfo = [...news];
+    let index = newsInfo.findIndex((news) => {
+      return news.id === id;
+    });
+    newsInfo[index] = { ...newsInfo[index], voteUp: parseInt(voteData[id]) };
+    setNews(newsInfo);
+  };
+
+  const visibleNewsList = news.filter((item) => !item.isHide);
+
+  return (
+    <main>
+      {visibleNewsList.length > 0 && (
+        <NewsDetail
+          news={visibleNewsList}
+          pagination={pagination}
+          onHideNews={hideNewsHandler}
+          onVoteUp={voteUpHandler}
+        ></NewsDetail>
+      )}
+      {visibleNewsList.length > 0 && (
+        <UpVoteChart news={visibleNewsList}></UpVoteChart>
+      )}
+    </main>
+  );
+}
+export default Home;
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Pagination(props) {
   const currentPage = props.pagination.currentPage;
   const numberOfPages = props.pagination.numberOfPages;
   return (
-    <div>
-      <a href="#" disabled={currentPage == 0}>
-        {"<<Previous"}
-      </a>
-      <a href="#" disabled={currentPage == numberOfPages}>
-        {"Next>>"}
-      </a>
+    <div className="pagination">
+      <Link
+        to={{
+          pathname: "/",
+          search: `?page=${currentPage - 1}`,
+        }}
+      >
+        &#60;&#60; Previous
+      </Link>
+      <Link
+        to={{
+          pathname: "/",
+          search: `?page=${currentPage + 1}`,
+        }}
+      >
+        Next &#62;&#62;
+      </Link>
     </div>
   );
 }
